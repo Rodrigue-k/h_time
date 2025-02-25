6f1dcfaa7246c60d592319e5be26a46aeff885f6
Komi Rodrigue KOUDAKPO <gabirusamaa@gmail.com>
2025-02-25 07:11:22 +0000

    Update calendar.dart

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,18 +1,121 @@
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class Calendar extends StatefulWidget {
-  const Calendar({super.key});
+class TimeTableScreen extends StatelessWidget {
+  const TimeTableScreen({super.key});
 
-  @override
-  State<Calendar> createState() => _CalendarState();
-}
-
-class _CalendarState extends State<Calendar> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color.fromARGB(255, 151, 19, 19),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Row(
+        children: [
+          Ruler(
+            width: 60,
+            hourHeight: 60,
+            lineColor: Colors.grey.shade400,
+            textColor: Colors.grey.shade700,
+          ),
+          Expanded(child: Container()),
+        ],
+      ),
     );
+  }
+}
+
+class Ruler extends StatelessWidget {
+  final double width;
+  final double hourHeight;
+  final Color lineColor;
+  final Color textColor;
+
+  const Ruler({
+    super.key,
+    this.width = 100,
+    this.hourHeight = 40,
+    this.lineColor = Colors.grey,
+    this.textColor = Colors.grey,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: SingleChildScrollView(
+        child: CustomPaint(
+          size: Size(100, 24 * hourHeight +20),
+          painter: RulerPaint(
+            hourHeight: hourHeight,
+            lineColor: lineColor,
+            textColor: textColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RulerPaint extends CustomPainter {
+  final double hourHeight;
+  final Color lineColor;
+  final Color textColor;
+
+  RulerPaint({
+    required this.hourHeight,
+    required this.lineColor,
+    required this.textColor,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint =
+        Paint()
+          ..color = lineColor
+          ..strokeWidth = 1;
+
+    final textPainter = TextPainter(textDirection: TextDirection.ltr);
+
+    for (int hour = 0; hour < 24; hour++) {
+      final y = hour * hourHeight +20;
+      canvas.drawLine(
+        Offset(size.width - 15, y),
+        Offset(size.width, y),
+        paint,
+      );
+
+      final quarterHeight = hourHeight / 4;
+      for (int i = 1; i < 4; i++) {
+        canvas.drawLine(
+          Offset(size.width - 10, y + (i * quarterHeight)),
+          Offset(size.width, y + (i * quarterHeight)),
+          paint,
+        );
+      }
+
+      final timeText = TextSpan(
+        text: '${hour.toString().padLeft(2, '0')}:00',
+        style: GoogleFonts.inter(
+          fontSize: 12,
+          color: textColor,
+          fontWeight: FontWeight.w500,
+        ),
+      );
+
+      textPainter
+        ..text = timeText
+        ..layout();
+
+      textPainter.paint(
+        canvas,
+        Offset(5, y - textPainter.height/2),
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant RulerPaint oldDelegate) {
+    return oldDelegate.hourHeight != hourHeight ||
+        oldDelegate.lineColor != lineColor ||
+        oldDelegate.textColor != textColor;
   }
 }
